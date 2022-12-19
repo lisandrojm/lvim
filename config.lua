1049882e931e4a824013fdfdf4e882ceb2ee9b81
lisandrojm <lisandrojm@gmail.com>
2022-12-19 12:17:22 -0300

    update

--- config.lua
+++ config.lua
@@ -74,15 +74,33 @@ formatters.setup({
 		command = "black",
 		extra_args = { "--fast" },
 	},
+
+	{ command = "shfmt", filetypes = { "sh", "zsh", "bash" } },
 })
+vim.filetype.add({
+	extension = {
+		zsh = "zsh",
+	},
+})
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "bashls" })
+
+local lsp_manager = require("lvim.lsp.manager")
+lsp_manager.setup("bashls", {
+	filetypes = { "sh", "zsh" },
+	on_init = require("lvim.lsp").common_on_init,
+	capabilities = require("lvim.lsp").common_capabilities(),
+})
+
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
 	{ command = "flake8", filetypes = { "python" } },
-	{
-		command = "shellcheck",
-		args = { "--severity", "warning" },
-	},
+	-- {
+	-- 	command = "shellcheck",
+	-- 	args = { "--severity", "warning" },
+	-- },
 })
+
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 -- lvim.plugins = {
 --     {
